7649851736b87952be32ddf9335dcb7f1e7c82aa
haluto <haluto8086@sina.cn>
2021-03-11 17:28:58 +0800

    collision detection

--- tetris/src/js/components/GameScreen.js
+++ tetris/src/js/components/GameScreen.js
@@ -7,6 +7,8 @@ const MAP_WIDTH = CONSTANT.GAME_PANEL.MAP_WIDTH;
 const MAP_HEIGHT = CONSTANT.GAME_PANEL.MAP_HEIGHT;
 const GRID_SIDE_LEN = CONSTANT.GAME_PANEL.GRID_SIDE_LEN;
 const MAP_STYLE_BORDER = CONSTANT.GAME_PANEL.MAP_STYLE_BORDER;
+const NEW_ITEM_DEFAULT_X = 4;
+const COLLISION_DETECTION_DIR = {LEFT:0, RIGHT:1, DOWN:2};
 
 export default class GameScreen extends React.Component {
   constructor(props) {
@@ -24,59 +26,123 @@ export default class GameScreen extends React.Component {
     this.mapData = [];
     this.curItem = null;
     this.nextItem = null;
-    this.prePos = {x:4, y:0};
-    this.curPos = {x:4, y:0};
+    this.prePos = {x:NEW_ITEM_DEFAULT_X, y:0};
+    this.curPos = {x:NEW_ITEM_DEFAULT_X, y:0};
 
     this.state = {
       status: CONSTANT.GAME_STATUS.NOT_START,
     };
   }
 
-  drawScreen = () => {
-    let data = this.curItem.getItem();
-    // reset previous item to empty.
-    for(let i=0;i<this.curItem.MAX_WIDTH;i++) {
-      for(let j=0;j<this.curItem.MAX_HEIGHT;j++) {
-        
-        this.mapData[i+this.prePos.x][j+this.prePos.y] = 0;
+  drawCurItem = (drawItem) => {
+    // drawItem === 0 means clean the item.
+    if (drawItem === 0) {
+      for(let i=0;i<this.curItem.MAX_WIDTH;i++) {
+        for(let j=0;j<this.curItem.MAX_HEIGHT;j++) {
+          let x = i+this.curPos.x;
+          let y = j+this.curPos.y;
+          this.drawGrid(0, x, y);
+        }
+      }
+    } else {
+      let data = this.curItem.getItem();
+      for(let i=0;i<this.curItem.MAX_WIDTH;i++) {
+        for(let j=0;j<this.curItem.MAX_HEIGHT;j++) {
+          let x = i+this.curPos.x;
+          let y = j+this.curPos.y;
+          this.drawGrid(data[i][j], x, y);
+        }
       }
     }
+  }
+
+  drawScreen = () => {
+    let data = this.curItem.getItem();
     // set current item to map.
     for(let i=0;i<this.curItem.MAX_WIDTH;i++) {
       for(let j=0;j<this.curItem.MAX_HEIGHT;j++) {
-        
-        this.mapData[i+this.curPos.x][j+this.curPos.y] = data[i][j];
+        if (data[i][j] === 1) {
+          this.mapData[i+this.curPos.x][j+this.curPos.y] = data[i][j];
+        }
       }
     }
 
     for (let i=0;i<MAP_WIDTH;i++) {
       for (let j=0;j<MAP_HEIGHT;j++) {
-        this.ctx.save();
-        if (this.mapData[i][j] === 1) {
-          this.ctx.fillStyle = "rgba(100, 100, 100, 1)";
-          this.ctx.strokeStyle = "rgba(0, 0, 0, 1)";
-        } else {
-          this.ctx.save();
-          this.ctx.fillStyle = "rgba(255, 255, 255, 1)";
-          // TODO: if need game help
-          //this.ctx.strokeStyle = "rgba(200, 200, 200, 0.5)";
-          this.ctx.strokeStyle = "rgba(255, 255, 255, 1)";
-        }
-        this.ctx.fillRect(i*GRID_SIDE_LEN+2, j*GRID_SIDE_LEN+2, GRID_SIDE_LEN-4, GRID_SIDE_LEN-4);
-        this.ctx.strokeRect(i*GRID_SIDE_LEN+1, j*GRID_SIDE_LEN+1, GRID_SIDE_LEN-2, GRID_SIDE_LEN-2);
-        this.ctx.restore();
+        this.drawGrid(this.mapData[i][j], i, j);
       }
     }
   }
 
+  drawGrid = (black, x, y) => {
+    this.ctx.save();
+    if (black) {
+      this.ctx.fillStyle = "rgba(100, 100, 100, 1)";
+      this.ctx.strokeStyle = "rgba(0, 0, 0, 1)";
+    } else {
+      this.ctx.fillStyle = "rgba(255, 255, 255, 1)";
+      // TODO: if need game help
+      //this.ctx.strokeStyle = "rgba(200, 200, 200, 0.5)";
+      this.ctx.strokeStyle = "rgba(255, 255, 255, 1)";
+    }
+    this.ctx.clearRect(x*GRID_SIDE_LEN, y*GRID_SIDE_LEN, GRID_SIDE_LEN, GRID_SIDE_LEN);
+    this.ctx.fillRect(x*GRID_SIDE_LEN+2, y*GRID_SIDE_LEN+2, GRID_SIDE_LEN-4, GRID_SIDE_LEN-4);
+    this.ctx.strokeRect(x*GRID_SIDE_LEN+1, y*GRID_SIDE_LEN+1, GRID_SIDE_LEN-2, GRID_SIDE_LEN-2);
+    this.ctx.restore();
+  }
+
   gameLoop = () => {
-    this.drawScreen();
-    //TODO: need check bottom.
-    this.prePos.x = this.curPos.x;
-    this.prePos.y = this.curPos.y;
-    this.curPos.y ++;
-    console.log("gameLoop");
+    if (this.curItem === null) {
+      let dir = getRandomInt(CONSTANT.ITEM_DIR.DIR_NUM);
+      // TODO: random item
+      this.curItem = new Item_L(dir);
+      this.curPos.x = NEW_ITEM_DEFAULT_X;
+      this.curPos.y = 0;
+      this.prePos.x = this.curPos.x;
+      this.prePos.y = this.curPos.y;
+      this.initData();
+      this.drawCurItem(1);
+    } else {
+      if (!this.doCollisionDetection(COLLISION_DETECTION_DIR.DOWN)) {
+        this.drawCurItem(0);
+        this.prePos.x = this.curPos.x;
+        this.prePos.y = this.curPos.y;
+        this.curPos.y ++;
+        this.drawCurItem(1);
+      } else { // TODO: check full line.
+        this.drawScreen();
+        let dir = getRandomInt(CONSTANT.ITEM_DIR.DIR_NUM);
+        // TODO: random item
+        this.curItem = new Item_L(dir);
+        this.curPos.x = NEW_ITEM_DEFAULT_X;
+        this.curPos.y = 0;
+        this.prePos.x = this.curPos.x;
+        this.prePos.y = this.curPos.y;
+        this.drawCurItem(1);
+      }
+    }
     setTimeout(this.gameLoop, 1000);
+  }
+
+  doCollisionDetection = (cdDir) => {
+    if (cdDir === COLLISION_DETECTION_DIR.DOWN) {
+      let ret = false;
+      let data = this.curItem.getItem()
+      for(let i=0;i<this.curItem.MAX_WIDTH;i++) {
+        for(let j=0;j<this.curItem.MAX_HEIGHT;j++) {
+          if (j+this.curPos.y < MAP_HEIGHT-1 &&
+                data[i][j] === 1 && 
+                this.mapData[i+this.curPos.x][j+this.curPos.y+1] === 1) {
+            ret = true;
+            break;
+          } else if (j+this.curPos.y === MAP_HEIGHT-1 && data[i][j] === 1) {
+            ret = true;
+            break;
+          }
+        }
+      }
+      return ret;
+    }
   }
 
   buttonClickCb = () => {
@@ -84,11 +150,6 @@ export default class GameScreen extends React.Component {
     if (status === CONSTANT.GAME_STATUS.NOT_START) {
       // start game
       status = CONSTANT.GAME_STATUS.RUNNING;
-      let dir = getRandomInt(CONSTANT.ITEM_DIR.DIR_NUM);
-      this.curItem = new Item_L(dir);
-      this.curPos.x = 4;
-      this.curPos.y = 0;
-      this.initData();
       this.gameLoop();
     } else if (status === CONSTANT.GAME_STATUS.RUNNING) {
       status = CONSTANT.GAME_STATUS.PAUSE;
